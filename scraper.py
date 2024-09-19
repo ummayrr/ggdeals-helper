40b0fc7a383215da24eeec67249c0f3844e794ce
Umair <umairnawaz184@gmail.com>
2024-09-20 03:39:10 +0500

    more tests

--- scraper.py
+++ scraper.py
@@ -1,46 +1,170 @@
 from bs4 import BeautifulSoup
 import requests
 import lxml
+import os
 
-gameName = input("Enter the game name: ")
+def basicFormatting (gameName):
+    gameName = gameName.replace(':','')
+    gameName = gameName.replace('-','')
+    gameName = gameName.replace(',','')
+    gameName = gameName.replace('+','')
+    gameName = gameName.replace('(','')
+    gameName = gameName.replace(')','')
+    gameName = gameName.replace('!','')
+    gameName = gameName.replace('.','')
+    return gameName
 
-#basic concatenation for game URL
+def formatName (gameName):
+    gameNameLower = gameName.lower()
+    splitted = gameNameLower.split()
+    listLength = len(splitted)
 
-gameNameLower = gameName.lower()
+    for i in range(listLength):
+        if(listLength == 1):
+            break
+        if(i==0):
+            splitted.insert(i+1,'-')
+        elif (i>1):
+            splitted.insert(i*2 - 1,'-')
+        i=i+1
 
-gameNameLower = gameNameLower.replace("'s"," s ")
-gameNameLower = gameNameLower.replace(':','')
-gameNameLower = gameNameLower.replace('-','')
+    updatedListLength = len(splitted)
+    gameNameWithDashes = ''.join(splitted)
+    return gameNameWithDashes
 
-splitted = gameNameLower.split()
-listLength = len(splitted)
+def test1(gameName):                            ### Assassin's Creed to Assassin-s-Creed
+    gameName = gameName.replace("'s"," s ")
+    gameName = formatName(gameName)
+    return gameName
 
-for i in range(listLength):
-    if(listLength == 1):
+def test2(gameName):                            ### Assassin's Creed to Assassins Creed
+    gameName = gameName.replace("'s","s")
+    gameName = formatName(gameName)
+    return gameName
+
+def test3(gameName):                            ### The Darkness II to The Darkness 2
+    gameNameList = gameName.split()
+    testStr = "Hello"
+
+    index = 0
+
+    for i in range(len(gameNameList)):
+        testStr = gameNameList[i]
+        if ((testStr[0] == 'I' or testStr[0] == 'i') and (testStr[-1] == 'I' or testStr[-1] == 'i')):
+            index = i
+            break
+
+    count = 0
+
+    if index == 0:
+        return gameName
+
+
+    for i in testStr:
+        count = count + 1
+
+    gameNameList[index] = str(count)
+
+    for i in range(len(gameNameList)):
+     if(len(gameNameList) == 1):
         break
-    if(i==0):
-        splitted.insert(i+1,'-')
-    elif (i>1):
-        splitted.insert(i*2 - 1,'-')
+     if(i==0):
+        gameNameList.insert(i+1,' ')
+     elif (i>1):
+        gameNameList.insert(i*2 - 1,' ')
+     i=i+1
+
+    gameName = ''.join(gameNameList)
+    gameName = formatName(gameName)
+    return gameName
+
+def test4(gameName):                            ### Dark Souls 3 to Dark Souls III
+    gameNameList = gameName.split()
+
+    testInt = 0
+    index = 0
+
+    for i in range(len(gameNameList)):
+        test = gameNameList[i]
+        try:
+            testInt = int(test)
+            if (testInt > 0):
+             index = i
+             break
+        except:
+            continue
+        i+=1
+
+    if (testInt == 0):
+        return gameName
+
+    romanStr = ""
+
+    for i in range(testInt):
+        romanStr += 'I'
+
+    gameNameList[index] = romanStr
+
+    for i in range(len(gameNameList)):
+     if(len(gameNameList) == 1):
+       break
+     if(i==0):
+       gameNameList.insert(i+1,' ')
+     elif (i>1):
+       gameNameList.insert(i*2 - 1,' ')
     i=i+1
 
-updatedListLength = len(splitted)
-gameNameWithDashes = ''.join(splitted)
+    gameName = ''.join(gameNameList)
+    gameName = formatName (gameName)
+    return gameName
 
-#create request to the URL 
+def makeRequest (gameName):
+    try:
+       url = "https://gg.deals/game/" + gameName
+#      print(url)
+       response = requests.get(url,timeout=10)
+       response.raise_for_status() 
+       return response
+    except requests.exceptions.RequestException as e:
+     print(f"Request failed: {e}")
+     return None
 
-print("\n")
-url = "https://gg.deals/game/" + gameNameWithDashes
+def makeSoup(response,inputNameForDisplay):
+    soup = BeautifulSoup(response.text, 'lxml')
 
-response = requests.get(url)
+    for data in soup.findAll('span',{'class':'price-inner numeric'}):
+        if('~' in data.text):
+            lowest = data.text
+            lowest = lowest.replace('~','')
+            print(inputNameForDisplay + ": " + lowest)
+            with open('results.txt','a') as f:
+                print(inputNameForDisplay + ": " + lowest, file = f)
+            break
 
-#soup
+def driver(inputName):
+    inputNameForDisplay = inputName
+    inputName = basicFormatting (inputName)
 
-soup = BeautifulSoup(response.text, 'lxml')
+    #create request to the URL 
+    response = makeRequest(test1(inputName))
+    if (response.status_code == 404):
+          response = makeRequest(test2(inputName))
+          if (response.status_code == 404):
+           response = makeRequest(test3(inputName))
+           if (response.status_code == 404):
+            response = makeRequest(test4(inputName))
+            if not response or response.status_code == 404:
+             print(f"Could not find the game: {inputNameForDisplay}")
+             return
 
-for data in soup.findAll('span',{'class':'price-inner numeric'}):
-    if('~' in data.text):
-        lowest = data.text
-        lowest = lowest.replace('~','')
-        print(gameName + ": " + lowest + "\n")
-        break
+
+    #soup
+
+    makeSoup(response,inputNameForDisplay)
+
+if(os.path.isfile('results.txt')):
+ os.remove('results.txt')
+with open('names.txt','r') as f:
+    for line in f:
+        driver(line.rstrip())
+        
--- scraper_specific.py
+++ scraper_specific.py
@@ -26,137 +26,259 @@ print("Kinguin prices are calculated without any voucher and fees added. Look in
 print("Lowest overall is considered with vouchers, any fees. The top one shown for keyshops on gg.deals page. \n")
 
 gg_csrf_token = os.getenv("csrf")
+gameName = ''
 
-gameName = input("Enter the game name: ")
+def basicFormatting (gameName):                          ### basic symbols removal
+    gameName = gameName.replace(':','')
+    gameName = gameName.replace('-','')
+    gameName = gameName.replace(',','')
+    gameName = gameName.replace('+','')
+    gameName = gameName.replace('(','')
+    gameName = gameName.replace(')','')
+    gameName = gameName.replace('!','')
+    gameName = gameName.replace('.','')
+    return gameName
 
-#basic concatenation for url 
+def formatName (gameName):                                 ### add dashes and convert to the main form for gg.deals/game/this-form-here from name sent in spaces
+    gameNameLower = gameName.lower()
+    splitted = gameNameLower.split()
+    listLength = len(splitted)
 
-gameNameLower = gameName.lower()
+    for i in range(listLength):
+        if(listLength == 1):
+            break
+        if(i==0):
+            splitted.insert(i+1,'-')
+        elif (i>1):
+            splitted.insert(i*2 - 1,'-')
+        i=i+1
 
-gameNameLower = gameNameLower.replace("'s"," s ")
-gameNameLower = gameNameLower.replace(':','')
-gameNameLower = gameNameLower.replace('-','')
+    updatedListLength = len(splitted)
+    gameNameWithDashes = ''.join(splitted)
+    return gameNameWithDashes
 
-splitted = gameNameLower.split()
-listLength = len(splitted)
+def test1(gameName):                            ### Assassin's Creed to Assassin-s-Creed
+    gameName = gameName.replace("'s"," s ")
+    gameName = formatName(gameName)
+    return gameName
 
-for i in range(listLength):
-    if(listLength == 1):
+def test2(gameName):                            ### Assassin's Creed to Assassins Creed
+    gameName = gameName.replace("'s","s")
+    gameName = formatName(gameName)
+    return gameName
+
+def test3(gameName):                            ### The Darkness II to The Darkness 2
+    gameNameList = gameName.split()
+    testStr = "Hello"
+
+    index = 0
+
+    for i in range(len(gameNameList)):
+        testStr = gameNameList[i]
+        if ((testStr[0] == 'I' or testStr[0] == 'i') and (testStr[-1] == 'I' or testStr[-1] == 'i')):
+            index = i
+            break
+
+    count = 0
+
+    if index == 0:
+        return gameName
+
+
+    for i in testStr:
+        count = count + 1
+
+    gameNameList[index] = str(count)
+
+    for i in range(len(gameNameList)):
+     if(len(gameNameList) == 1):
         break
-    if(i==0):
-        splitted.insert(i+1,'-')
-    elif (i>1):
-        splitted.insert(i*2 - 1,'-')
+     if(i==0):
+        gameNameList.insert(i+1,' ')
+     elif (i>1):
+        gameNameList.insert(i*2 - 1,' ')
+     i=i+1
+
+    gameName = ''.join(gameNameList)
+    gameName = formatName(gameName)
+    return gameName
+
+def test4(gameName):                            ### Dark Souls 3 to Dark Souls III
+    gameNameList = gameName.split()
+
+    testInt = 0
+    index = 0
+
+    for i in range(len(gameNameList)):
+        test = gameNameList[i]
+        try:
+            testInt = int(test)
+            if (testInt > 0):
+             index = i
+             break
+        except:
+            continue
+        i+=1
+
+    if (testInt == 0):
+        return gameName
+
+    romanStr = ""
+
+    for i in range(testInt):
+        romanStr += 'I'
+
+    gameNameList[index] = romanStr
+
+    for i in range(len(gameNameList)):
+     if(len(gameNameList) == 1):
+       break
+     if(i==0):
+       gameNameList.insert(i+1,' ')
+     elif (i>1):
+       gameNameList.insert(i*2 - 1,' ')
     i=i+1
 
-updatedListLength = len(splitted)
-gameNameWithDashes = ''.join(splitted)
+    gameName = ''.join(gameNameList)
+    gameName = formatName (gameName)
+    return gameName
 
-url = "https://gg.deals/game/" + gameNameWithDashes
+def makeRequest (gameName):
+    url = "https://gg.deals/game/" + gameName
+    print(url)
+    response = requests.get(url)
+    return response
 
-#creating response and soup
+def initialSoup (response):                      #required for getting gameID mainly
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup
 
-response = requests.get(url)
+def findGameID (soup1):                        #finding gameID for post request, (POST for the load more option, to get to kinguin if it is hidden below load more options)
+    divs = soup1.findAll('div')
 
-soup = BeautifulSoup(response.text, 'lxml')
+    gameID = 0
 
-lowestPrice = ''
+    for div in divs:
+        gameID = div.get('data-container-game-id')
+        if (gameID is None):
+            continue;   
+        intID = int(gameID)             
+        if (intID > 0):
+            gameID = intID
+            break
 
-#find the major keyshops lowest price shown on top, get the first one and break
+    url = f"https://gg.deals/us/games/keyshopsDeals/{gameID}/"
+    
+    #creating header for new request
 
-for data in soup.findAll('span',{'class':'price-inner numeric'}):
-    if('~' in data.text):
-        lowestPrice = data.text
-        lowestPrice = lowestPrice.replace('~','')
-        break
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
+        "Referer": f"https://gg.deals/game/" + gameName,  
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Requested-With": "XMLHttpRequest", 
+    }
 
-#finding gameID for post request, (POST for the load more option, to get to kinguin if it is hidden below load more options)
+    #from payload
 
-divs = soup.findAll('div')
+    data = {
+        "gg_csrf": gg_csrf_token  
+    }
 
-gameID = 0
+    cookies = {
+        "cookie": os.getenv("cookie")
+    }
 
-for div in divs:
-    gameID = div.get('data-container-game-id')
-    if (gameID is None):
-        continue;   
-    intID = int(gameID)             
-    if (intID > 0):
-        gameID = intID
-        break
+    #posting
 
-url = f"https://gg.deals/us/games/keyshopsDeals/{gameID}/"
+    response = requests.post(url, headers=headers, data=data, cookies=cookies)
 
-#creating header for new request
+    if response.status_code != 200:
+        print(f"Failed. Status code: {response.status_code}, Response: {response.text}")
+        
+    soup = BeautifulSoup(response.text, 'lxml')
+    return soup
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
-    "Referer": f"https://gg.deals/game/" + gameName,  
-    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-    "X-Requested-With": "XMLHttpRequest", 
-}
 
-#from payload
+def kinguinExtractor(soup1, soup2, inputNameForDisplay):                  ### soup 1 is only passed to pass into final function
 
-data = {
-    "gg_csrf": gg_csrf_token  
-}
+    
+    #kinguin div
 
-cookies = {
-    "cookie": os.getenv("cookie")
-}
+    list_ = []
 
-#posting
+    kinguin_shop = soup2.find('div', {'data-shop-name': 'Kinguin'})
 
-response = requests.post(url, headers=headers, data=data, cookies=cookies)
+    kinguin_shop_str = str(kinguin_shop)
 
-if response.status_code != 200:
-    print(f"Failed. Status code: {response.status_code}, Response: {response.text}")
+    kinguin_shop_str = kinguin_shop_str.replace('&lt;','<')
+    kinguin_shop_str = kinguin_shop_str.replace('&gt;','>')
 
-#soup 
+    #pull-right finds the 3 values, w/ voucher, w/o, w/ voucher & fee
 
-soup = BeautifulSoup(response.text, 'lxml')
+    span_values = re.findall(r'<span class="pull-right">(.*?)</span>', kinguin_shop_str)
 
-#kinguin div
+    for data in span_values:
+            list_.append(data)
 
-list_ = []
+    #list gets all 3 values, in list[0],[1],[2]
 
-kinguin_shop = soup.find('div', {'data-shop-name': 'Kinguin'})
+    list_.sort()
 
-kinguin_shop_str = str(kinguin_shop)
+    c = CurrencyConverter()
+    kinguinPriceFormatted = list_[1].replace('$','')
 
-kinguin_shop_str = kinguin_shop_str.replace('&lt;','<')
-kinguin_shop_str = kinguin_shop_str.replace('&gt;','>')
+    kinguinPrice = float(kinguinPriceFormatted)
 
-with open('a.txt','w') as f:
-        print(kinguin_shop, file=f)
+    #0.35 eur fixed fee to usd
 
-#pull-right finds the 3 values, w/ voucher, w/o, w/ voucher & fee
+    fixed35fee = c.convert(0.35, 'EUR', 'USD')
+    kinguinPrice = kinguinPrice - fixed35fee
+    kinguinPrice = kinguinPrice - (kinguinPrice * 0.14)         # iwtr = base price - 0.35 eur        # iwtr = iwtr - (14% of iwtr)
 
-span_values = re.findall(r'<span class="pull-right">(.*?)</span>', kinguin_shop_str)
+    iwtrFinal = format(kinguinPrice , '.2f') # CHANGES FLOAT NUMBER TO STRING AUTOMATICALLY, AND ROUNDING OFF by 2 DECIMAL PLACES
+    iwtrFinal = '$' + iwtrFinal
+    lowestOnKinguin = list_[1]
+    final(soup1, inputNameForDisplay, lowestOnKinguin, iwtrFinal)
+         
+def printer(inputNameForDisplay, lowestPrice, lowestOnKinguin, iwtrFinal):          ## printing all output
+    with open('results.txt','a') as f:
+     print(inputNameForDisplay + ":", file = f)
+     print("Lowest overall: " + lowestPrice, file = f)
+     print("Lowest on Kinguin: " + lowestOnKinguin, file = f)
+     print("IWTR: " + iwtrFinal + "\n", file = f)   
 
-for data in span_values:
-        list_.append(data)
+def final(soup1, inputNameForDisplay, lowestOnKinguin, iwtrFinal):      ## finds lowest price overall, and passes to print function everything
+    lowestPrice = ''
 
-#list gets all 3 values, in list[0],[1],[2]
+    for data in soup1.findAll('span',{'class':'price-inner numeric'}):
+        if('~' in data.text):
+            lowestPrice = data.text
+            lowestPrice = lowestPrice.replace('~','')
+            break
+     
+    printer(inputNameForDisplay, lowestPrice, lowestOnKinguin, iwtrFinal)    
 
-list_.sort()
+def driver(inputName):                                             ### main driver
+    inputNameForDisplay = inputName
+    inputName = basicFormatting (inputName)
+    #create request to the URL 
+    response = makeRequest(test1(inputName))
+    if (response.status_code == 404):
+          response = makeRequest(test2(inputName))
+          if (response.status_code == 404):
+           response = makeRequest(test3(inputName))
+           if (response.status_code == 404):
+            response = makeRequest(test4(inputName))
+    
+    soup1 = initialSoup(response)
+    secondSoup = findGameID(soup1)
+    kinguinExtractor(soup1, secondSoup, inputNameForDisplay)
+    
+     ### actual main ###
 
-c = CurrencyConverter()
-kinguinPriceFormatted = list_[1].replace('$','')
+if(os.path.isfile('results.txt')):
+ os.remove('results.txt')
 
-kinguinPrice = float(kinguinPriceFormatted)
-
-#0.35 eur fixed fee to usd
-
-fixed35fee = c.convert(0.35, 'EUR', 'USD')
-kinguinPrice = kinguinPrice - fixed35fee
-kinguinPrice = kinguinPrice - (kinguinPrice * 0.14)         # iwtr = base price - 0.35 eur        # iwtr = iwtr - (14% of iwtr)
-
-iwtrFinal = format(kinguinPrice , '.2f') # CHANGES FLOAT NUMBER TO STRING AUTOMATICALLY, AND ROUNDING OFF by 2 DECIMAL PLACES
-iwtrFinal = '$' + iwtrFinal
-
-print(gameName + ":")
-print("Lowest overall: " + lowestPrice)
-print("Lowest on Kinguin: " + list_[1])
-print("IWTR: " + iwtrFinal + "\n")        
+with open('names.txt','r') as f:
+  for line in f:
+    driver(line.rstrip())
